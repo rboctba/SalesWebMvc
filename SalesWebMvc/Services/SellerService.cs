4170fb92880b1cbd920f45b2b6f7c815785c8bdc
rboctba <rbocasa@yahoo.com.br>
2023-10-10 19:34:09 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -9,12 +9,12 @@ namespace SalesWebMvc.Services
 
         public SellerService(SalesWebMvcContext context)
         {
-            _context = context; 
+            _context = context;
         }
 
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList();    
+            return _context.Seller.ToList();
         }
 
         public void Insert(Seller obj)
@@ -22,5 +22,18 @@ namespace SalesWebMvc.Services
             _context.Add(obj);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
     }
 }
