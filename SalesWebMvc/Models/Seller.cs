fd3a01192f8f23efc2c76286faa3e7ec0fb60390
rboctba <rbocasa@yahoo.com.br>
2023-10-13 16:20:56 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -7,20 +7,29 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         [Display(Name = "#")] public int Id { get; set; }
-        [AllowNull][Display(Name = "Nome")] public string Name { get; set; }
-        [Display(Name = "e-Mail")][DataType(DataType.EmailAddress)] public string? Email { get; set; }
+        [Required(ErrorMessage ="{0} é obrigatório")]
+        [StringLength(60,MinimumLength = 3,ErrorMessage = "{0} precisa ter um tamanho entre {2} e {1}")]
+
+        [Display(Name = "Nome")] public string Name { get; set; }
+        [Display(Name = "e-Mail")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="entre com um email valido!")]
+
+        public string? Email { get; set; }
         [DataType(DataType.Date)]
-        [Display(Name = "Data Aviversário")]
+        [Display(Name = "Data de nascimento")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
         [Display(Name = "Salário base")]
         [DataType(DataType.Currency)]
+        [Required(ErrorMessage = "{0} é obrigatório")]
         //[DisplayFormat(DataFormatString = "{0:F2}")] 
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve ser entre {1} to {2}")]
         public double BaseSalary { get; set; }
         [AllowNull] public Department Department { get; set; }
         [Display(Name = "Departamento")] public int DepartmentId { get; set; }
         [Display(Name = "Vendas")] public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
