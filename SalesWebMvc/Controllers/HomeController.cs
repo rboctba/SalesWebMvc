ca063a7f7fb91f4889b84751421cb42ab4ee4535
rboctba <rbocasa@yahoo.com.br>
2023-10-13 17:25:06 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -8,12 +8,6 @@ namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using SalesWebMvc.Services;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
@@ -7,19 +8,20 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         [Display(Name = "#")] public int Id { get; set; }
-        [Required(ErrorMessage ="{0} é obrigatório")]
-        [StringLength(60,MinimumLength = 3,ErrorMessage = "{0} precisa ter um tamanho entre {2} e {1}")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} precisa ter um tamanho entre {2} e {1}")]
 
-        [Display(Name = "Nome")] public string Name { get; set; }
+        [Display(Name = "Nome")]
+        public string Name { get; set; } = string.Empty;
+
         [Display(Name = "e-Mail")]
         [DataType(DataType.EmailAddress)]
-        [EmailAddress(ErrorMessage ="entre com um email valido!")]
-
+        [EmailAddress(ErrorMessage = "entre com um email valido!")]
         public string? Email { get; set; }
         [DataType(DataType.Date)]
         [Display(Name = "Data de nascimento")]
         [Required(ErrorMessage = "{0} é obrigatório")]
-        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
         [Display(Name = "Salário base")]
         [DataType(DataType.Currency)]
@@ -27,9 +29,16 @@ namespace SalesWebMvc.Models
         //[DisplayFormat(DataFormatString = "{0:F2}")] 
         [Range(100.0, 50000.0, ErrorMessage = "{0} deve ser entre {1} to {2}")]
         public double BaseSalary { get; set; }
-        [AllowNull] public Department Department { get; set; }
-        [Display(Name = "Departamento")] public int DepartmentId { get; set; }
-        [Display(Name = "Vendas")] public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        [Display(Name = "combo do Departamento")]
+
+        public Department? Department { get; set; } 
+
+        [Display(Name = "Departamento")]        
+        public int DepartmentId { get; set; }
+
+        [Display(Name = "Vendas")]
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
